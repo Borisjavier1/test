29c252e9e73e0e7a0577bbb5611748b2d4d9337c
BORIS MONGE CHAVES <boris.monge.chaves@est.una.ac.cr>
2022-06-16 18:21:46 -0600

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.example.test
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.toolbox.JsonObjectRequest
+import com.google.gson.Gson
+import okhttp3.*
+import okhttp3.RequestBody
+import org.json.JSONObject
+
+import java.io.IOException
+
+
+class MainActivity : AppCompatActivity() {
+    var client = OkHttpClient()
+    var url = "http://192.168.0.2:8080/backend_moviles/api/sistema/"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var button2 = findViewById<Button>(R.id.button2)
+
+        button2.setOnClickListener {
+          // run()
+            runpost()
+        }
+    }
+
+    fun run() {
+       // val etLocation = findViewById<EditText>(R.id.etLocation)
+        val request = Request.Builder()
+            //.url("http://10.0.2.2:28019/api/usuarios")
+            .url(url+"obtenerAlumno")
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println(e.message.toString())
+                //Toast.makeText(applicationContext,e.message.toString(),Toast.LENGTH_SHORT).show()
+            }
+            override fun onResponse(call: Call, responseHttp: okhttp3.Response) {
+                val gson = Gson()
+                var valor = responseHttp.body()?.string()
+                println(valor)
+                //Toast.makeText(applicationContext,valor.toString(),Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun runpost() {
+
+
+        // Post parameters
+        // Form fields and values
+        // Form fields and values
+        val jsonObject = JSONObject()
+        jsonObject.put("carrera",21)
+        jsonObject.put("cedula",705)
+        jsonObject.put("email","lslsl@gmail.com")
+        jsonObject.put("fecha_nac","22-22-22")
+        jsonObject.put("id",88)
+        jsonObject.put("nombre","karl")
+        jsonObject.put("telefono","22-22-22")
+
+
+
+        // Volley post request with parameters
+        val request = JsonObjectRequest(com.android.volley.Request.Method.POST,url+"insertarAlumno",jsonObject,
+            { response ->
+                // Process the json
+                try {
+                   // etCord.setText("Response: $response")
+                    println(response)
+                }catch (e:Exception){
+                    //etClima.setText("Exception: $e")
+                    println(e)
+                }
+
+            }, {
+                // Error in request
+              //  etHumedad.setText("Volley error: $it")
+                println("Error reques: t"+it)
+            })
+
+
+        // Volley request policy, only one time request to avoid duplicate transaction
+        request.retryPolicy = DefaultRetryPolicy(
+            DefaultRetryPolicy.DEFAULT_TIMEOUT_MS,
+            // 0 means no retry
+            0, // DefaultRetryPolicy.DEFAULT_MAX_RETRIES = 2
+            1f // DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        )
+        // Add the volley post request to the request queue
+        VolleySingleton.getInstance(this).addToRequestQueue(request)
+    }
+
+}
---
+++ app/src/main/java/com/example/test/ya.kt
@@ -0,0 +1,10 @@
+package com.example.test
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+import kotlin.Throws
+
+class ya {
+
+}
